5936a1b7b6d239df488e3af1a7438d4ea71fe0d0
Amit Rajgor <amitdrajgor@gmail.com>
2026-06-20 09:21:53 +0530

    modified car not starting css and html file

---
+++ js/car not starting.js
@@ -0,0 +1,19 @@
+﻿const questions = document.getElementById("question");
+const yesBtn = document.getElementById("yes-btn");
+const noBtn = document.getElementById("no-btn");
+const video = document.getElementById("diagnosis-video");
+const videoSource = document.getElementById("video-source");
+
+yesBtn.addEventListener("click", (function() {
+    questions.textContent = "there must be problem related to immobilizer system, fuel system, ignition system or engine got damaged. lets check one by one where the problem is.";
+    videoSource.src = "../videos/immobilizer.mp4";
+    video.load();
+    video.play();
+}));
+
+noBtn.addEventListener("click", (function() {
+    questions.textContent = "there must be problem related to battery, starter motor or wiring. lets check one by one where the problem is.";
+    videoSource.src = "../videos/battery.mp4";
+    video.load();
+    video.play();
+}));
